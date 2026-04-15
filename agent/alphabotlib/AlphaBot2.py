abfbe6268bc6887ea454c6901aa422ca155a1f09
mariethoz <54270350+mariethoz@users.noreply.github.com>
2026-04-15 10:26:08 +0200

    feat: add sensor and robot camera base
    - the line sensor use TRSensors
    - leds use rpi_ws281x
    - the camera use picamera2 actually not tested

--- agent/alphabotlib/AlphaBot2.py
+++ agent/alphabotlib/AlphaBot2.py
@@ -1,106 +1,168 @@
 import RPi.GPIO as GPIO
 import time
 
+from TRSensors import TRSensor
+from rpi_ws281x import Adafruit_NeoPixel, Color
+
+from picamera2 import Picamera2
+from io import BytesIO
+
 class AlphaBot2(object):
-	def __init__(self,ain1=12,ain2=13,ena=6,bin1=20,bin2=21,enb=26):
-		self.AIN1 = ain1
-		self.AIN2 = ain2
-		self.BIN1 = bin1
-		self.BIN2 = bin2
-		self.ENA = ena
-		self.ENB = enb
-		self.PA  = 50
-		self.PB  = 50
+    def __init__(self,ain1=12,ain2=13,ena=6,bin1=20,bin2=21,enb=26):
+        self.AIN1 = ain1
+        self.AIN2 = ain2
+        self.BIN1 = bin1
+        self.BIN2 = bin2
+        self.ENA = ena
+        self.ENB = enb
+        self.PA  = 50
+        self.PB  = 50
 
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setwarnings(False)
-		GPIO.setup(self.AIN1,GPIO.OUT)
-		GPIO.setup(self.AIN2,GPIO.OUT)
-		GPIO.setup(self.BIN1,GPIO.OUT)
-		GPIO.setup(self.BIN2,GPIO.OUT)
-		GPIO.setup(self.ENA,GPIO.OUT)
-		GPIO.setup(self.ENB,GPIO.OUT)
-		self.PWMA = GPIO.PWM(self.ENA,500)
-		self.PWMB = GPIO.PWM(self.ENB,500)
-		self.PWMA.start(self.PA)
-		self.PWMB.start(self.PB)
-		self.stop()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.AIN1,GPIO.OUT)
+        GPIO.setup(self.AIN2,GPIO.OUT)
+        GPIO.setup(self.BIN1,GPIO.OUT)
+        GPIO.setup(self.BIN2,GPIO.OUT)
+        GPIO.setup(self.ENA,GPIO.OUT)
+        GPIO.setup(self.ENB,GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA,500)
+        self.PWMB = GPIO.PWM(self.ENB,500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()
 
-	def forward(self):
-		self.PWMA.ChangeDutyCycle(self.PA)
-		self.PWMB.ChangeDutyCycle(self.PB)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.HIGH)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.HIGH)
+        # Add infrared obstacle avoidance sensors
+        self.DR = 16
+        self.DL = 19
+        GPIO.setup(self.DR,GPIO.IN,GPIO.PUD_UP)
+        GPIO.setup(self.DL,GPIO.IN,GPIO.PUD_UP)
 
+        # Add line sensor
+        self.tr_sensor = TRSensor()
 
-	def stop(self):
-		self.PWMA.ChangeDutyCycle(0)
-		self.PWMB.ChangeDutyCycle(0)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.LOW)
+        # LED strip configuration:
+        self.LED_COUNT      = 4      # Number of LED pixels.
+        LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+        LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+        LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+        LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+        LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+        LED_CHANNEL    = 0
 
-	def backward(self):
-		self.PWMA.ChangeDutyCycle(self.PA)
-		self.PWMB.ChangeDutyCycle(self.PB)
-		GPIO.output(self.AIN1,GPIO.HIGH)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.HIGH)
-		GPIO.output(self.BIN2,GPIO.LOW)
+        self.strip = Adafruit_NeoPixel(self.LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 
-		
-	def left(self):
-		self.PWMA.ChangeDutyCycle(30)
-		self.PWMB.ChangeDutyCycle(30)
-		GPIO.output(self.AIN1,GPIO.HIGH)
-		GPIO.output(self.AIN2,GPIO.LOW)
-		GPIO.output(self.BIN1,GPIO.LOW)
-		GPIO.output(self.BIN2,GPIO.HIGH)
+        self.strip.begin()
+        self.strip.setPixelColor(0, Color(0, 0, 0))       #Red
+        self.strip.setPixelColor(1, Color(0, 0, 0))       #Green
+        self.strip.setPixelColor(2, Color(0, 0, 0))       #Blue
+        self.strip.setPixelColor(3, Color(0, 0, 0))       #Yellow
+        self.strip.show()
 
+        # Configure camera
+        self.picam = Picamera2()
+        config = self.picam.create_still_configuration()
+        self.picam.configure(config)
 
-	def right(self):
-		self.PWMA.ChangeDutyCycle(30)
-		self.PWMB.ChangeDutyCycle(30)
-		GPIO.output(self.AIN1,GPIO.LOW)
-		GPIO.output(self.AIN2,GPIO.HIGH)
-		GPIO.output(self.BIN1,GPIO.HIGH)
-		GPIO.output(self.BIN2,GPIO.LOW)
-		
-	def setPWMA(self,value):
-		self.PA = value
-		self.PWMA.ChangeDutyCycle(self.PA)
+    def forward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.AIN1,GPIO.LOW)
+        GPIO.output(self.AIN2,GPIO.HIGH)
+        GPIO.output(self.BIN1,GPIO.LOW)
+        GPIO.output(self.BIN2,GPIO.HIGH)
 
-	def setPWMB(self,value):
-		self.PB = value
-		self.PWMB.ChangeDutyCycle(self.PB)	
-		
-	def setMotor(self, left, right):
-		if((right >= 0) and (right <= 100)):
-			GPIO.output(self.AIN1,GPIO.HIGH)
-			GPIO.output(self.AIN2,GPIO.LOW)
-			self.PWMA.ChangeDutyCycle(right)
-		elif((right < 0) and (right >= -100)):
-			GPIO.output(self.AIN1,GPIO.LOW)
-			GPIO.output(self.AIN2,GPIO.HIGH)
-			self.PWMA.ChangeDutyCycle(0 - right)
-		if((left >= 0) and (left <= 100)):
-			GPIO.output(self.BIN1,GPIO.HIGH)
-			GPIO.output(self.BIN2,GPIO.LOW)
-			self.PWMB.ChangeDutyCycle(left)
-		elif((left < 0) and (left >= -100)):
-			GPIO.output(self.BIN1,GPIO.LOW)
-			GPIO.output(self.BIN2,GPIO.HIGH)
-			self.PWMB.ChangeDutyCycle(0 - left)
+    def stop(self):
+        self.PWMA.ChangeDutyCycle(0)
+        self.PWMB.ChangeDutyCycle(0)
+        GPIO.output(self.AIN1,GPIO.LOW)
+        GPIO.output(self.AIN2,GPIO.LOW)
+        GPIO.output(self.BIN1,GPIO.LOW)
+        GPIO.output(self.BIN2,GPIO.LOW)
+
+    def backward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.AIN1,GPIO.HIGH)
+        GPIO.output(self.AIN2,GPIO.LOW)
+        GPIO.output(self.BIN1,GPIO.HIGH)
+        GPIO.output(self.BIN2,GPIO.LOW)
+
+    def left(self):
+        self.PWMA.ChangeDutyCycle(30)
+        self.PWMB.ChangeDutyCycle(30)
+        GPIO.output(self.AIN1,GPIO.HIGH)
+        GPIO.output(self.AIN2,GPIO.LOW)
+        GPIO.output(self.BIN1,GPIO.LOW)
+        GPIO.output(self.BIN2,GPIO.HIGH)
+
+    def right(self):
+        self.PWMA.ChangeDutyCycle(30)
+        self.PWMB.ChangeDutyCycle(30)
+        GPIO.output(self.AIN1,GPIO.LOW)
+        GPIO.output(self.AIN2,GPIO.HIGH)
+        GPIO.output(self.BIN1,GPIO.HIGH)
+        GPIO.output(self.BIN2,GPIO.LOW)
+        
+    def setPWMA(self,value):
+        self.PA = value
+        self.PWMA.ChangeDutyCycle(self.PA)
+
+    def setPWMB(self,value):
+        self.PB = value
+        self.PWMB.ChangeDutyCycle(self.PB)	
+        
+    def setMotor(self, left, right):
+        if((right >= 0) and (right <= 100)):
+            GPIO.output(self.AIN1,GPIO.HIGH)
+            GPIO.output(self.AIN2,GPIO.LOW)
+            self.PWMA.ChangeDutyCycle(right)
+        elif((right < 0) and (right >= -100)):
+            GPIO.output(self.AIN1,GPIO.LOW)
+            GPIO.output(self.AIN2,GPIO.HIGH)
+            self.PWMA.ChangeDutyCycle(0 - right)
+        if((left >= 0) and (left <= 100)):
+            GPIO.output(self.BIN1,GPIO.HIGH)
+            GPIO.output(self.BIN2,GPIO.LOW)
+            self.PWMB.ChangeDutyCycle(left)
+        elif((left < 0) and (left >= -100)):
+            GPIO.output(self.BIN1,GPIO.LOW)
+            GPIO.output(self.BIN2,GPIO.HIGH)
+            self.PWMB.ChangeDutyCycle(0 - left)
+
+    def get_ioa(self):
+        DR_status = GPIO.input(self.DR)
+        DL_status = GPIO.input(self.DL)
+        return DR_status, DL_status
+
+    def get_tr_value(self):
+        return self.tr_sensor.AnalogRead()
+    
+    def set_led(self, led_id: int, color: Color):
+        strip.setPixelColor(led_id%self.LED_COUNT, color)
+        strip.show()
+
+    def get_photo(self):
+        self.picam.start()
+        time.sleep(2) # Adjust time
+
+        buffer = BytesIO()
+        self.picam.capture_file(buffer, format="jpeg")
+        self.picam.stop()
+
+        buffer.seek(0)
+        jpeg_bytes = buffer.read()
+
+        # Encode to base64 for XMPP
+        return base64.b64encode(jpeg_bytes).decode()
+
 
 if __name__=='__main__':
 
-	Ab = AlphaBot2()
-	Ab.forward()
-	try:
-		while True:
-			time.sleep(1)
-	except KeyboardInterrupt:
-		GPIO.cleanup()
+    Ab = AlphaBot2()
+    Ab.forward()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
